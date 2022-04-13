b4ed5900530ff89d2aac4f55e831fac3228b31fd
Gabriel <gabrielariasmarin@gmail.com>
2022-04-13 19:49:02 -0400

    Create rangeQ, include projDB files

---
+++ rangeQ.cpp
@@ -0,0 +1,43 @@
+//rangeQ
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+
+vector<int> sequential()
+{
+    
+
+
+
+    return 
+}
+
+
+
+
+
+
+
+void main(searchOption)
+{
+
+    ifstream inFile;
+
+
+
+    switch(searchOption)
+    {
+        case 0: //sequential search
+        break;
+
+        case 1: //kdtree
+        break;
+
+        case 2: //MyKDtree
+        break;
+    }
+    
+}
